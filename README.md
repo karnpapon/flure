3898a3b75420a9cbcb995b64b78860dc7c89ed3c
Karnpapon Boonput <karnpapon@gmail.com>
2023-06-12 16:48:36 +0700

    add compiler mode

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ my attempt to learn lua language by implement FORTH interpreter.
 
 ## run
 - `lua main.lua`
+
+## resources
+- https://beza1e1.tuxen.de/articles/forth.html
--- main.lua
+++ main.lua
@@ -1,6 +1,10 @@
 local readline = require("readline")
 
+-- GLOBAL
+
 int_stack = {}
+compile_flag = false
+compile_words = ""
 
 -- ------------------------------------------------------------------------------------
 -- UTILS
@@ -38,6 +42,48 @@ function add()
   table.insert(int_stack, sum)
   print("add: " .. tostring(sum))
 end
+
+function subtract()
+  local top_word = table.remove(int_stack, #int_stack)
+  local second_word = table.remove(int_stack, #int_stack)
+  local diff = second_word - top_word
+  table.insert(int_stack, diff)
+  print("diff: " .. tostring(diff))
+end
+
+function multiply()
+  local top_word = table.remove(int_stack, #int_stack)
+  local second_word = table.remove(int_stack, #int_stack)
+  local product = top_word * second_word
+  table.insert(int_stack, product)
+  print("product: " .. tostring(product))
+end
+
+function divide()
+  local top_word = table.remove(int_stack, #int_stack)
+  local second_word = table.remove(int_stack, #int_stack)
+  local divisor = second_word / top_word
+  table.insert(int_stack, divisor)
+  print("divisor: " .. tostring(divisor))
+end
+
+function pop()
+  local top_word = table.remove(int_stack, #int_stack)
+  print("pop: " .. tostring(top_word))
+end
+
+function start_compile()
+  compile_flag = true
+  compile_words = compile_words .. "::::"
+end
+
+function compile(input) compile_words = compile_words .. " " .. input end
+
+function end_compile()
+  compile_words = compile_words .. " ;;;; "
+  compile_flag = false
+end
+
 -- ------------------------------------------------------------------------------------
 -- MAIN READ/EVAL/PRINT
 -- ------------------------------------------------------------------------------------
@@ -51,16 +97,42 @@ end
 
 function EVAL(input_array)
   for i, v in ipairs(input_array) do
-    if tonumber(v) ~= nil then
-      table.insert(int_stack, tonumber(v))
+    if compile_flag then
+      if v == "compiler" then
+        print("compile_words: " .. compile_words)
+      elseif v == ";" then
+        end_compile()
+      else
+        compile(v)
+      end
     else
-      if v == "show" then
-        print_r(int_stack)
-      elseif v == "bye" then
-        print("bye!")
-        return false
-      elseif v == "+" then
-        add()
+      if tonumber(v) ~= nil then
+        table.insert(int_stack, tonumber(v))
+      else
+        if v == "show" then
+          print_r(int_stack)
+        elseif v == "bye" then
+          print("bye!")
+          return false
+        elseif v == "+" then
+          add()
+        elseif v == "-" then
+          subtract()
+        elseif v == "*" then
+          multiply()
+        elseif v == "/" then
+          divide()
+        elseif v == "pop" then
+          pop()
+        elseif v == ":" then
+          start_compile()
+        elseif v == "compiler" then
+          print("compile_words: " .. compile_words)
+        elseif v == ";" then
+          print("not in compile mode")
+        elseif compile_words:find(":::: " .. v) then
+          print("exists in dict")
+        end
       end
     end
   end
